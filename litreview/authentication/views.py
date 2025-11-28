1a81ede0a765065baf86a8a0c924cc018c99b7e6
anselmlys <anselm.lisa@gmail.com>
2025-11-28 14:49:07 +0100

    feat: create signup page

--- litreview/authentication/views.py
+++ litreview/authentication/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 
 from . import forms
 
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    return render(request, 'authentication/signup.html', context={'form': form})
 
 def login_page(request):
     form = forms.LoginForm()
--- litreview/litreview/urls.py
+++ litreview/litreview/urls.py
@@ -22,4 +22,5 @@ import authentication.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', authentication.views.login_page, name='login'),
+    path('signup/', authentication.views.signup_page, name='signup'),
 ]
