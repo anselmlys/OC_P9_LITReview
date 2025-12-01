690af858a6a40baa3ea92acaeb74a8ba586099bb
anselmlys <anselm.lisa@gmail.com>
2025-12-01 16:12:04 +0100

    feat: create base CSS and CSS for login page

--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -4,8 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='Nom d\'utilisateur')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+    username = forms.CharField(max_length=63,
+                               label='',
+                               widget=forms.TextInput(attrs={'placeholder': 'Nom d\'utilisateur'}))
+    password = forms.CharField(max_length=63,
+                               label='',
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}))
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
