f8d7158d0d90545bec77814586b8478b75646939
anselmlys <anselm.lisa@gmail.com>
2025-12-02 17:12:56 +0100

    feat: create CSS for ticket creation page

--- litreview/flux/forms.py
+++ litreview/flux/forms.py
@@ -4,6 +4,12 @@ from . import models
 
 
 class TicketForm(forms.ModelForm):
+    title = forms.CharField(label='Titre')
+
     class Meta:
         model = models.Ticket
         fields = ['title', 'description', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
