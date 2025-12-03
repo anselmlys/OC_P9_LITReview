16e4bea45d7b2d8c5493cf01ec6bb432b839c8d7
anselmlys <anselm.lisa@gmail.com>
2025-12-03 18:33:10 +0100

    feat: create CSS for review creation page (in response to existing ticket)

--- litreview/flux/forms.py
+++ litreview/flux/forms.py
@@ -16,6 +16,23 @@ class TicketForm(forms.ModelForm):
 
 
 class ReviewForm(forms.ModelForm):
+    CHOICES = [
+        (0, '0'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    headline = forms.CharField(label='Titre')
+    rating = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
+    body = forms.CharField(widget=forms.Textarea, label='Commentaire')
+
     class Meta:
         model = models.Review
         fields = ['headline', 'body', 'rating']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
